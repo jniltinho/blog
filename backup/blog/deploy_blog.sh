de3ea7f04b5af49c6e74e7fa773cbcfe96df3845
Nilton Oliveira <jniltinho@gmail.com>
2022-09-30 18:28:34 -0300

    Update deploy_blog.sh

--- backup/blog/deploy_blog.sh
+++ backup/blog/deploy_blog.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -f db.json
-rm -rf public/*
+rm -rf public/* node_modules
+
+npm install
 hexo generate
 
 ### Site Static for Github Pages
@@ -11,12 +13,12 @@ hexo generate
 ## git commit -m "Update Posts on Blog"
 ## git push origin gh-pages
 
+## Update Raw Blog to GitHub Repo
+#git add *
+#git commit -m "Update Raw Blog to Github"
+#git push origin master
 
 ### Site Static for Google APP Engine
-## gcloud auth login
-gcloud -q app deploy --project bloglinuxpro
-
-## Update Raw Blog to GitHub Repo
-git add *
-git commit -m "Update Raw Blog to Github"
-git push origin master
+## GCloud Auth
+echo $GCP_CREDENTIALS|gcloud auth activate-service-account --key-file=-
+gcloud -q --project=bloglinuxpro app deploy app.yaml -v=v$(date +'%Y%m%d-%H%M') --promote
